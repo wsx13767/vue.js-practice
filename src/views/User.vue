c5d972c8841b2abf8800320e32fc31bdb7041d9c
siang <wsx13767@gmail.com>
2021-08-02 21:01:56 +0800

    display params,query

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        <p>User Id: {{ $route.params.id }}</p>
-        <p>User Name: {{$route.query.userName}}</p><!-- 可取得url?後的參數a=1&b=2 -->
-        <p>Message: {{message}}</p><!-- router的path 開啟props後，可使用props接收$route.params的參數 -->
-    </div>
+  <div>
+    <p>User Id: {{ $route.params.id }}</p>
+    <p>User Name: {{ $route.query.userName }}</p>
+    <!-- 可取得url?後的參數a=1&b=2 -->
+    <p>Message: {{ message }}</p>
+    <!-- router的path 開啟props後，可使用props接收$route.params的參數 -->
+    <p>params: {{ $route.params }}</p>
+    <p>query: {{ $route.query }}</p>
+  </div>
 </template>
 <script>
 export default {
-    props: ['message'],
-    created() {
-        console.log(this.message);
-        console.log(this.$route.params)
-        console.log(this.$route.query)
-    }
-}
+  props: ["message"],
+  created() {
+    console.log(this.message);
+    console.log(this.$route.params);
+    console.log(this.$route.query);
+  },
+};
 </script>
